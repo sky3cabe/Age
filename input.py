0c8ca5208305d0a15bfb2125bba02cebb62b0d2c
sky3cabe <53106293+sky3cabe@users.noreply.github.com>
2019-10-20 18:11:14 +0800

    This is just a code
    Please fork me -No pressure

---
+++ input.py
@@ -0,0 +1,19 @@
+def lol() :
+	if age > 5 :
+		print ("You are still kids.You should not go outside without your parents permission.")
+    if age > 12 :
+    	print ("You are started to be a teenager.Good luck.")
+    if age > 18 :
+    	print ("You are now a teenager.You still need your parents permission to go outside.")
+    if age > 25 :
+    	print ("You are an adult.")
+    if age > 40 : 
+    	print ("You are started to be an old people.So fighting because you need to becareful of your health.")
+    if age > 59 :
+    	print ("You are now an old people.")
+
+# this is for the input 
+age=float (input("What is your age :"))
+
+# this is for the output
+age=lol()
